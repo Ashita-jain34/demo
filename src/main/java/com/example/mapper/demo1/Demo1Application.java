53383df807f5aa0a9be5f39d3fbe3ca5d82c29fe
Ashita Jain <jain.ashita@upgrad.com>
2020-10-31 19:29:14 +0530

    updated main method

--- src/main/java/com/example/mapper/demo1/Demo1Application.java
+++ src/main/java/com/example/mapper/demo1/Demo1Application.java
@@ -1,7 +1,11 @@
 package com.example.mapper.demo1;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Demo1Application {
@@ -9,5 +13,8 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
-
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
